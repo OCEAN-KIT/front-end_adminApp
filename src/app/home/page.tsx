704978ef53a1f643590b67ae569aa66c98d12780
Ryu Tae Hyun <ryu200112@gmail.com>
2025-10-13 16:28:14 +0900

    Feat: 웹푸시 연결

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,12 @@
+import PushInit from "@/components/PushInit";
+import DebugPushPanel from "@/components/DebugPanel";
+
 export default function HomePage() {
   return (
     <div>
       <div>home.</div>
+      <DebugPushPanel />
+      <PushInit />
     </div>
   );
 }
